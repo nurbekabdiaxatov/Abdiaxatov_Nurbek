6c9c9831d53bbf5dabdad6bed57a993bb7e18f67
Nurbek <161306161+nurbek2107@users.noreply.github.com>
2024-11-17 22:51:50 +0500

    man
    Signed-off-by: Nurbek <161306161+nurbek2107@users.noreply.github.com>

--- src/components/particleimage.tsx
+++ src/components/particleimage.tsx
@@ -35,7 +35,7 @@ const ParticleDemo = () => {
     }, []);
 
     return (
-        <div className="relative">
+        <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
             <img
                 id="logo"
                 ref={logoRef}
