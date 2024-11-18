06448300389a74893cbda39e5e2f897b242395ef
Nurbek <161306161+nurbek2107@users.noreply.github.com>
2024-11-18 21:56:51 +0500

    MAIN
    Signed-off-by: Nurbek <161306161+nurbek2107@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,6 @@
+// Add this at the top of your file to ensure the component is treated as client-side
+"use client";
+
 import Navbar from "@/components/navbar";
 import Speed from "@/components/Speed";
 import CustomCursor from "@/components/CustomCursor";
@@ -8,69 +11,40 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
-
+import { motion, useScroll } from "framer-motion";
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
-
-export const metadata: Metadata = {
-  metadataBase: new URL(DATA.url),
-  title: {
-    default: DATA.name,
-    template: `%s | ${DATA.name}`,
-  },
-  description: DATA.description,
-  openGraph: {
-    title: `${DATA.name}`,
-    description: DATA.description,
-    url: DATA.url,
-    siteName: `${DATA.name}`,
-    locale: "en_US",
-    type: "website",
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
-  twitter: {
-    title: `${DATA.name}`,
-    card: "summary_large_image",
-  },
-  verification: {
-    google: "",
-    yandex: "",
-  },
-};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // Move the scroll logic inside the client component
+  const { scrollYProgress } = useScroll();
+
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/N.png" />
-      </head> 
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased ",
           fontSans.variable
         )}
       >
+        <motion.div
+          className="progress-bar"
+          style={{ scaleX: scrollYProgress }}
+        />
         <ThemeProvider attribute="class" defaultTheme="light">
           <TooltipProvider delayDuration={0}>
             <CustomCursor /> {/* Move CustomCursor inside here */}
-            {children } 
+            {children}
             <div className="pointer-events-none fixed inset-x-0 bottom-0 z-50 mx-auto mb-4 flex origin-bottom h-full max-h-14 items-center gap-20 justify-center">
               <Navbar />
               <Speed />
